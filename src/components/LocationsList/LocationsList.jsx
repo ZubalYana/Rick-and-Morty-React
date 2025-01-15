babb4d6864a484dbe628162eb2ac4bd9eb762d36
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-15 19:35:49 +0200

    create the locations paginations as well

--- src/components/LocationsList/LocationsList.jsx
+++ src/components/LocationsList/LocationsList.jsx
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './LocationsList.css';
 import axios from 'axios';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 export default function LocationsList() {
   const [locations, setLocations] = useState([]);
+  const [pageInfo, setPageInfo] = useState({ next: null, prev: null, pages: 0 });
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-  function fetchLocations() {
+  const currentPage = parseInt(searchParams.get('page')) || 1;
+
+  useEffect(() => {
+    fetchLocations(currentPage);
+  }, [currentPage]);
+
+  function fetchLocations(page) {
     axios
-      .get('https://rickandmortyapi.com/api/location')
+      .get(`https://rickandmortyapi.com/api/location?page=${page}`)
       .then((response) => {
-        console.log('API response:', response.data.results);
+        console.log('API response:', response.data);
         setLocations(response.data.results);
+        setPageInfo({
+          next: response.data.info.next,
+          prev: response.data.info.prev,
+          pages: response.data.info.pages,
+        });
       })
       .catch((error) => {
         console.error('Error fetching locations:', error);
       });
   }
 
-  useEffect(() => {
-    fetchLocations();
-  }, []);
+  const handlePageChange = (page) => {
+    navigate(`/locations?page=${page}`);
+  };
 
   return (
     <div>
@@ -36,7 +51,7 @@ export default function LocationsList() {
         <tbody>
           {locations.map((location, index) => (
             <tr key={location.id}>
-              <td>{index + 1}</td>
+              <td>{index + 1 + (currentPage - 1) * 20}</td>
               <td>{location.name}</td>
               <td>{location.type}</td>
               <td>{location.dimension}</td>
@@ -44,6 +59,21 @@ export default function LocationsList() {
           ))}
         </tbody>
       </table>
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={!pageInfo.prev}
+        >
+          Previous
+        </button>
+        <span>Page {currentPage} of {pageInfo.pages}</span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={!pageInfo.next}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
