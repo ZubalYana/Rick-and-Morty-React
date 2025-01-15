2a1e5667fc52b64cb2a87158852294533ce5b4f8
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-15 05:43:16 +0200

    fix bugs with navigation and add basic styles for the main page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import './HomePage.css'
+import React from 'react';
+import './HomePage.css';
+import backgroundPoster from '/backgroundPoster.avif';
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="Homepage screen">
+      <div className="backgroundPoster_con">
+        <img src={backgroundPoster} alt="backgroundPoster" className="backgroundPoster" />
+        <div className="backgroundPoster_overlay"></div>
+      </div>
+    </div>
+  );
 }
