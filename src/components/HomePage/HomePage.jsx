2151278c386b090eaf11159cca712234de09849f
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-17 05:40:17 +0200

    add new styles to the homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './HomePage.css';
-import backgroundPoster from '/backgroundPoster.avif';
-
+import rickAndMortyLogo from '/rick and morty img.jpg';
 export default function HomePage() {
   return (
     <div className="Homepage screen">
-      <div className="backgroundPoster_con">
-        <img src={backgroundPoster} alt="backgroundPoster" className="backgroundPoster" />
-        <div className="backgroundPoster_overlay"></div>
+      <div className="Homepage_content">
+        <div className="textContainer">
+          <h1 className='homePage_title'>The best Rick and Morty funpage ever!</h1>
+          <p className='homePage_text'>Browse through series characters and discover tons of fascinating details about each of them. Explore the complete list of episodes and locations from the movie series! Additionally, log episodes in your watchlist and stay on track with your Rick and Morty journey.</p>
+        </div>
+        <div className="homepage_imgCon">
+          <img src={rickAndMortyLogo} alt="rickAndMortyLogo" className="rickAndMortyLogo" />
+        </div>
       </div>
     </div>
   );
