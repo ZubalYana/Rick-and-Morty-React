dddcb8e4a38cf9fe3a668ac0121e76b5613cb377
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-18 09:56:58 +0200

    clean the previous unused code

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './HomePage.css';
-import rickAndMortyLogo from '/rick and morty img.jpg';
 import RickAndMortyIllustration from '../RickAndMortyIllustration/RickAndMortyIllustration';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -13,7 +12,7 @@ export default function HomePage() {
           <h1 className="homePage_title">Rick and Morty funpage №1</h1>
           <p className="homePage_text">
           <p>Explore a comprehensive list of episodes, from iconic adventures to hidden gems, and discover the bizarre and unpredictable locations featured throughout the series. Whether it's the Citadel of Ricks, Anatomy Park, or the mysterious Planet Squanch, there's always something new to learn and love!</p>
-          <p style={{ marginTop: '5px' }}>Create your personal watchlist to keep track of the episodes you've seen and the ones still on your journey. Relive your favorite moments, stay updated, and never miss a beat as you traverse the insane, action-packed world of Rick and Morty.</p>
+          <p style={{ marginTop: '8px' }}>Create your personal watchlist to keep track of the episodes you've seen and the ones still on your journey. Relive your favorite moments, stay updated, and never miss a beat as you traverse the insane, action-packed world of Rick and Morty.</p>
           </p>
           <div className="homepageBtns">
           <button className="exploreButton homepageButton">
@@ -26,7 +25,6 @@ export default function HomePage() {
 
         </div>
         <div className="homepage_imgCon">
-          {/* <img src={rickAndMortyLogo} alt="Rick and Morty Logo" className="rickAndMortyLogo" /> */}
           <RickAndMortyIllustration />
         </div>
       </div>
